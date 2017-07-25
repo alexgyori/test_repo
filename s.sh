addd98305ca09662b81d23107352624a55506224
August Shi <awshi2@illinois.edu>
2017-07-25 15:15:16 -0500

    More advanced cache playing

--- s.sh
+++ s.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
+mkdir -p module1/target
+mkdir -p module2/target
+if [[ -e module1/target/.surefire-foobar ]]; then
+    cat module1/target/.surefire-foobar
+fi
+cat >> module1/target/.surefire-foobar << EOF
+abcdefg
+EOF
+if [[ -e module2/target/.surefire-foobaz ]]; then
+    cat module2/target/.surefire-foobaz
+fi
+cat >> module2/target/.surefire-foobaz << EOF
+abc
+efg
+hij
+EOF
+exit
+
+"""
 echo "START!"
 ls foo/
 echo "LINE" >> foo/f
 echo "CHECKING CONTENTS OF F"
 cat foo/f
+"""
